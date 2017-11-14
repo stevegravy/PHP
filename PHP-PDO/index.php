2d675f1ab819fc10604b3b0d892a70cdb43091ad
user <contact@stevegravy.com>
2017-11-14 16:46:00 +0100

    pdo presque fini

--- PHP-PDO/index.php
+++ PHP-PDO/index.php
@@ -22,39 +22,55 @@
 			die('Erreur : '.$e->getMessage());
 	}
 
-	$resultat = $bdd->query(' SELECT * FROM Météo');
+	$resultat = $bdd->query("SELECT * FROM meteo");
 	$donnees = $resultat->fetch();
-
 	while ($donnees = $resultat->fetch()){
-		
-?>
-	
-	
-	<p>
-		Ville : <?php echo $donnees['ville']; ?> <br>
-		Minima: <?php echo $donnees['bas']; ?> <br>
-		Maxima: <?php echo $donnees['haut']; ?> <br>
-	</p>
-	
-<?php
-$resultat->closeCursor();
+		echo "<table>";
+		//On affiche les données dans le tableau
+    
+        echo "<tr><td>";
+        echo $donnees['ville'];
+        echo '</td><td>';
+        echo $donnees['bas'];
+        echo '</td><td>';
+        echo $donnees['haut'];
+        echo '</td></tr>';
+      
+	}
+	echo "</table>";
+
+	$resultat->closeCursor();
+
 ?>
 
-
-	<!---
 	<form method="post" action="">
 
-   	<p>
-   		<label>Entrez une ville :</label><input type="text" name="ville" /><br>
-   		<label>Température minimale</label><input type="number" name="bas" /><br>
-   		<label>Température maximale</label><input type="number" name="bas" /><br>
-   		
-   		<button action = "submit">Submit</button>
-   		
-	</p>
-   	
+		<p>
+			<label>Entrez une ville :</label><input type="text" name="ville" /><br>
+			<label>Température minimale</label><input type="number" name="bas" /><br>
+			<label>Température maximale</label><input type="number" name="bas" /><br>
+
+			<button action = "submit">Submit</button>
+
+		</p>
+
 
 	</form>
-	----->
+	
+
+<?php
+	
+	$ville = $_POST['ville'];
+	$bas = $_POST['bas'];
+	$haut = $_POST['haut'];
+	
+	$ville->exec('INSERT INTO meteo(ville)');
+	$basville->exec('INSERT INTO meteo(bas)');
+	$haut->exec('INSERT INTO meteo(haut)');
+?>
+	
+	
 </body>
 </html>
+
+
